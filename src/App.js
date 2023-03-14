34d37d6a710666def715938052edb62ce5ed81f8
Mllara <97680447+Mllara@users.noreply.github.com>
2023-03-13 23:06:10 -0300

    aula sete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import HelloWord from './Components/HelloWorld';
 import './App.css';
 import SayMyName from './Components/SayMyName';
-import Pessoa from './Components/Pessoa'
-import Frase from  './Components/Frase'
+import Pessoa from './Components/Pessoa';
+import Frase from  './Components/Frase';
+import List from './Components/List';
 
 function App() {
   const nome = 'Maria'
@@ -13,7 +14,14 @@ function App() {
         <Frase/>
         <SayMyName nome="Márcia"/>
         <SayMyName nome={nome} />
-        <Pessoa nome="Rodrigo"  idade="28" profissão="Programador" foto="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fportuguese%2Fgeral-37268918&psig=AOvVaw2CQr6qc9PstUIzRPPZ9DjE&ust=1678563688863000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDropeP0v0CFQAAAAAdAAAAABAE" />
+        <Pessoa 
+        nome="Rodrigo"  
+        idade="28" 
+        profissão="Programador" 
+        foto="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fportuguese%2Fgeral-37268918&psig=AOvVaw2CQr6qc9PstUIzRPPZ9DjE&ust=1678563688863000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDropeP0v0CFQAAAAAdAAAAABAE" 
+        
+        />
+        <List />
 
     </div>
   );
