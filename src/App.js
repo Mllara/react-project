cb23a4d8c4e6044351a2f575543cbf147733f191
Mllara <97680447+Mllara@users.noreply.github.com>
2023-03-14 22:02:56 -0300

    aula nove

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
-import HelloWord from './Components/HelloWorld';
 import './App.css';
-import SayMyName from './Components/SayMyName';
-import Pessoa from './Components/Pessoa';
-import Frase from  './Components/Frase';
-import List from './Components/List';
+import Evento from './Components/Eventos'
+
 
 function App() {
   const nome = 'Maria'
   return (
     <div className="App">
-      <h1>Testando</h1>
-        <Frase/>
-        <Frase/>
-        <SayMyName nome="Márcia"/>
-        <SayMyName nome={nome} />
-        <Pessoa 
-        nome="Rodrigo"  
-        idade="28" 
-        profissão="Programador" 
-        foto="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.bbc.com%2Fportuguese%2Fgeral-37268918&psig=AOvVaw2CQr6qc9PstUIzRPPZ9DjE&ust=1678563688863000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCPDropeP0v0CFQAAAAAdAAAAABAE" 
-        
-        />
-        <List />
+      <h1>Testando Eventos</h1>
+      <Evento numero="1"/>
+      <Evento numero="2"/>
 
     </div>
   );
