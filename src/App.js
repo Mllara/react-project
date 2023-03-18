cb84e71985230bdbd8edfeeb33a8bcee1f1ebf17
Mllara <97680447+Mllara@users.noreply.github.com>
2023-03-18 09:11:48 -0300

    aula nove

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Evento from './Components/Eventos'
+import Form from './Components/Form'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <h1>Testando Eventos</h1>
       <Evento numero="1"/>
       <Evento numero="2"/>
+      <Form />
 
     </div>
   );
